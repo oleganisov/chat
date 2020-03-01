dcb18535c390561a3e739dde31a5dbcbbe360fb8
Oleg V. Anisov <anisov@jet.su>
2020-03-01 23:54:00 +0300

    css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import '../assets/css/_styles.scss';
 
-const authForm = document.querySelector('#form-auth');
+const auth = document.querySelector('.auth');
+const wrapper = document.querySelector('.wrapper');
 const btnSubmit = document.querySelector('.form__submit');
+const authForm = document.querySelector('#form-auth');
 const userName = authForm.user;
 const userNick = authForm.nick;
 
 btnSubmit.addEventListener('click', e => {
     e.preventDefault();
+    auth.style.display = 'none';
+    wrapper.style.display = 'block';
     console.log(userName.value, userNick.value);
 });
