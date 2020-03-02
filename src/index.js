c8a5d9c5a3f12b14fc7e9c8005969bfb36343f7a
Oleg V. Anisov <anisov@jet.su>
2020-03-02 13:10:06 +0300

    add draft load file

--- src/index.js
+++ src/index.js
@@ -9,6 +9,25 @@ const authForm = document.querySelector('#form-auth');
 const userName = authForm.user;
 const userNick = authForm.nick;
 
+const fileReader = new FileReader();
+const chatPhoto = document.querySelector('#chatPhoto');
+const myAvatar = document.querySelector('#myAvatar');
+
+chatPhoto.addEventListener('change', e => {
+    const file = e.target.files[0];
+
+    if (file) {
+        if (file.size > 300 * 1024) {
+            alert('Слишком большой файл');
+        } else {
+            fileReader.readAsDataURL(file);
+        }
+    }
+});
+fileReader.addEventListener('load', () => {
+    myAvatar.src = fileReader.result;
+});
+
 btnSubmit.addEventListener('click', e => {
     e.preventDefault();
     if (userName.value && userNick.value) {
