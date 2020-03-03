ec6f7a4eb31eb52c9e25ad91e3702bfe47514346
Oleg V. Anisov <anisov@jet.su>
2020-03-03 18:34:29 +0300

    up

--- src/js/file.js
+++ src/js/file.js
@@ -2,20 +2,24 @@ const file = () => {
     const fileReader = new FileReader();
     const avatarPhoto = document.querySelector('#avatarPhoto');
     const myAvatar = document.querySelector('#myAvatar');
+    const photoImg = document.querySelector('.photo__img');
+
+    const overlay = document.querySelector('.photo_overlay');
 
     avatarPhoto.addEventListener('change', e => {
         const file = e.target.files[0];
 
         if (file) {
-            if (file.size > 300 * 1024) {
+            if (file.size > 500 * 1024) {
                 alert('Слишком большой файл');
             } else {
                 fileReader.readAsDataURL(file);
+                overlay.style.display = 'block';
             }
         }
     });
     fileReader.addEventListener('load', () => {
-        myAvatar.src = fileReader.result;
+        photoImg.src = fileReader.result;
     });
 };
 
