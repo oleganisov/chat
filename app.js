b6f75bbfd3116314736f0df09cf2685ab91f7b7e
Олег В. Анисов <anisov@jet.su>
2020-03-04 10:34:02 +0300

    uncomment in app js

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-    // socket.on('chat message', function(msg) {
-    //     io.emit('chat message', msg);
-    // });
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+    });
     console.log('a user connected');
 });
 
